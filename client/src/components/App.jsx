adff9072dd24e2fc092e868c234dbd39e7c2d4fe
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-27 19:31:50 -0700

    Add components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import CheckoutForm from './CheckoutForm';
+
+class App extends Component {
+  render() {
+    return (
+      <StripeProvider apiKey="pk_test_pQhnuyRSReWhY1em9BsAasjo00RX9j436Y">
+        <div className="example">
+          <h1>React Stripe Elements Example</h1>
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </StripeProvider>
+    );
+  }
+}
+
+export default App;
